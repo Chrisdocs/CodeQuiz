5f1cb8db324705541d9434923239c185b1ad2f08
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-04-08 14:32:22 -0500

    HTML - index.html main structure

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,6 +1,10 @@
+
 //when the start quiz button is clicked, the current section should be replaced with the first question
 //each question should have a time limit of 60 seconds (60,000 ms)
 //when a questions answer is selected, it should log a true or false and replace the section with the html for the new question
 //each new question should display a boolean value for the previous question at the bottom
 //once all questions have been answered, or the timer on a question reaches 0, the user is prensented with thier score
 //the option to input user initials should be availble alongside the highscore, and both should be stored locally
+
+//create an array which stores questions
+//create html block which can be referenced and modified by the array position to present the proper question
