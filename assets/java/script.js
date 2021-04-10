47f820e1874fe533053ad412ab3338c6699ab4ee
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-04-10 15:37:21 -0500

    all questions cycle through properly

--- assets/java/script.js
+++ assets/java/script.js
@@ -16,6 +16,7 @@ var quizStart = [
 
 
 var currentIndex = 0;
+var highscoreValue = 0;
     
 function answerCheck(userAnswer) {
     console.log("userAnswer: ", userAnswer);
@@ -30,9 +31,13 @@ function answerCheck(userAnswer) {
     //if user answer equals the correct answer then true, if not false
     if (userAnswer === correctAnswer) {
         console.log("correct!")
+        highscoreValue++;
+        console.log(highscoreValue);
     } else {
         console.log("incorrect!");
     }
+
+    currentIndex++;
     // for (var i = 0; i < quizStart.length; i++) {
     //     var answerChosen = quizStart[0].answers[i];
     //     var correctResponse = quizStart[i].correctAnswer;
@@ -41,33 +46,46 @@ function answerCheck(userAnswer) {
     // }
 };
 
+function startQuestionsButton() {
 
+    var mainDiv = document.getElementById("maindiv");
 
-//when the start quiz button is clicked, the current section should be replaced with the first question
-document.getElementById("stbtn").addEventListener("click", function startQuizButton() {
-    
-    var divEl = document.getElementById("qbox");
-    var btnEl = document.getElementById("btnbx");
-    var startEl = document.getElementById("homestart")
+    var divSecondary = document.createElement("div");
+    divSecondary.className = "div-secondary";
+    divSecondary.setAttribute("id", "divsecondary");
+
+    var titleSection = document.createElement("h1");
+    titleSection.className = "title-section";
+    titleSection.setAttribute("id", "setattribute");
+    titleSection.textContent = quizStart[currentIndex].question;
+
+    mainDiv.appendChild(divSecondary);
+
+    var divEl = document.createElement("div");
+    divEl.className = "div-el";
+    divEl.setAttribute("id", "divel");
+
+    divSecondary.appendChild(divEl);
+    divEl.appendChild(titleSection);
+
+    var btnEl = document.createElement("div");
+    btnEl.className = "btn-el"
+    btnEl.setAttribute("id", "btnel")
+    divEl.appendChild(btnEl);
+
     
     var btnDivA = document.createElement("div");
     var btnDivB = document.createElement("div");
     var btnDivC = document.createElement("div");
     var btnDivD = document.createElement("div");
-
-    var titleSection = document.createElement("h1");
-    var textSection = document.createElement("p");
+    
 
     var abtn = document.createElement("button")
     var bbtn = document.createElement("button")
     var cbtn = document.createElement("button")
     var dbtn = document.createElement("button")
-    
-    //replaced the initial welcome and start button with the DOM html for first question
-    divEl.parentNode.replaceChild(titleSection, divEl);
-    startEl.remove("homestart");
-    
-    //HTML DOM structure for questions
+
+
     btnEl.appendChild(btnDivA);
     btnEl.appendChild(btnDivB);
     btnEl.appendChild(btnDivC);
@@ -83,29 +101,22 @@ document.getElementById("stbtn").addEventListener("click", function startQuizBut
     btnDivD.className = "questions-buttons-d";
     
     abtn.className = "a-btn";
-    abtn.textContent = quizStart[0].answers.a;
+    abtn.textContent = quizStart[currentIndex].answers.a;
     abtn.setAttribute("id", "answerA");
     
     bbtn.className = "b-btn";
-    bbtn.textContent = quizStart[0].answers.b;
+    bbtn.textContent = quizStart[currentIndex].answers.b;
     bbtn.setAttribute("id", "answerB");
     
     cbtn.className = "c-btn";
-    cbtn.textContent = quizStart[0].answers.c;
+    cbtn.textContent = quizStart[currentIndex].answers.c;
     cbtn.setAttribute("id", "answerC");
     
     dbtn.className = "d-btn";
-    dbtn.textContent = quizStart[0].answers.d;
+    dbtn.textContent = quizStart[currentIndex].answers.d;
     dbtn.setAttribute("id", "answerD");
     
-    titleSection.className = "h1-box";
-    titleSection.textContent = quizStart[0].question;
-    
-    textSection.className = "question-text";
-    textSection.textContent = "";
-
-    //when each answer button is clicked, the console should return a true or fasle on whether it represents the correct answer, followed by the presentation of the next question
- 
+    // when each answer button is clicked, the console should return a true or fasle on whether it represents the correct answer, followed by the presentation of the next question
     document.getElementById("answerA").addEventListener("click", function clickedA() {
         console.log("clicked");
         var htmlElement = document.getElementById('answerA');
@@ -113,8 +124,10 @@ document.getElementById("stbtn").addEventListener("click", function startQuizBut
         console.log('testValue: ', testValue);
         console.log('typeof: ', typeof testValue);
         answerCheck(testValue);
+        mainDiv.removeChild(divSecondary);
+        startQuestionsButton();
     });
-
+    
     document.getElementById("answerB").addEventListener("click", function clickedB() {
         console.log("clicked");
         console.log("clicked");
@@ -123,8 +136,10 @@ document.getElementById("stbtn").addEventListener("click", function startQuizBut
         console.log('testValue: ', testValue);
         console.log('typeof: ', typeof testValue);
         answerCheck(testValue);
+        mainDiv.removeChild(divSecondary);
+        startQuestionsButton();
     });
-
+    
     document.getElementById("answerC").addEventListener("click", function clickedC() {
         console.log("clicked");
         console.log("clicked");
@@ -133,8 +148,10 @@ document.getElementById("stbtn").addEventListener("click", function startQuizBut
         console.log('testValue: ', testValue);
         console.log('typeof: ', typeof testValue);
         answerCheck(testValue);
+        mainDiv.removeChild(divSecondary);
+        startQuestionsButton();
     });
-
+    
     document.getElementById("answerD").addEventListener("click", function clickedD() {
         console.log("clicked");
         console.log("clicked");
@@ -143,7 +160,63 @@ document.getElementById("stbtn").addEventListener("click", function startQuizBut
         console.log('testValue: ', testValue);
         console.log('typeof: ', typeof testValue);
         answerCheck(testValue);
+        mainDiv.removeChild(divSecondary);
+        startQuestionsButton();
     });
+}
+
+
+
+//when the start quiz button is clicked, the current section should be replaced with the first question
+document.getElementById("stbtn").addEventListener("click", function startQuizButton() {
+    
+    var divMain = document.getElementById("maindiv");
+    var divBox = document.getElementById("divbox");
+
+    divMain.removeChild(divBox);
+
+    startQuestionsButton();
+    
+    //replaced the initial welcome and start button with the DOM html for first question
+    
+    //HTML DOM structure for questions
+    // btnEl.appendChild(btnDivA);
+    // btnEl.appendChild(btnDivB);
+    // btnEl.appendChild(btnDivC);
+    // btnEl.appendChild(btnDivD);
+    
+    // btnDivA.appendChild(abtn);
+    // btnDivA.className = "questions-buttons-a";
+    // btnDivB.appendChild(bbtn);
+    // btnDivB.className = "questions-buttons-b";
+    // btnDivC.appendChild(cbtn);
+    // btnDivC.className = "questions-buttons-c";
+    // btnDivD.appendChild(dbtn);
+    // btnDivD.className = "questions-buttons-d";
+    
+    // abtn.className = "a-btn";
+    // abtn.textContent = quizStart[currentIndex].answers.a;
+    // abtn.setAttribute("id", "answerA");
+    
+    // bbtn.className = "b-btn";
+    // bbtn.textContent = quizStart[currentIndex].answers.b;
+    // bbtn.setAttribute("id", "answerB");
+    
+    // cbtn.className = "c-btn";
+    // cbtn.textContent = quizStart[currentIndex].answers.c;
+    // cbtn.setAttribute("id", "answerC");
+    
+    // dbtn.className = "d-btn";
+    // dbtn.textContent = quizStart[currentIndex].answers.d;
+    // dbtn.setAttribute("id", "answerD");
+    
+    // titleSection.className = "h1-box";
+    // titleSection.textContent = quizStart[currentIndex].question;
+    
+    // textSection.className = "question-text";
+    // textSection.textContent = "";
+
+
 });
 
 //each question should have a time limit of 60 seconds (60,000 ms)
