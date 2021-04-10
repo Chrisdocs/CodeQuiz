7cb4804b755d2c5a46db5a81ce83ec21f89dc5be
Chris Wolfe <chrisa.wolfe@gmail.com>
2021-04-10 08:54:30 -0500

    re organized code, inital start button activation replaced HTML with DOM elements and inputs array values

--- assets/java/script.js
+++ assets/java/script.js
@@ -2,13 +2,13 @@
 var quizStart = [
     
     {question: "To select an element in your HTML via JavaScript you would use which method?", answers: { a: "createElement()", b: "querySlector()", c: "getElementById()", d: "appendChild()"}, correctAnswer: "c" },
-
+    
     {question: "test1", answers: { a: "one", b: "two", c: "three", d: "four"}, correctAnswer: "a" },
-
+    
     {question: "test2", answers: { a: "one", b: "two", c: "three", d: "four"}, correctAnswer: "d" },
-
+    
     {question: "test3", answers: { a: "one", b: "two", c: "three", d: "four"}, correctAnswer: "b" },
-
+    
     {question: "This is a question?", answers: { a: "one", b: "two", c: "three", d: "four"}, correctAnswer: "c" },
 ];
 
@@ -20,10 +20,10 @@ document.getElementById("stbtn").addEventListener("click", function startQuizBut
     var btnEl = document.getElementById("btnbx");
     var startEl = document.getElementById("homestart")
     
-    var qBtnA = document.createElement("div");
-    var qBtnB = document.createElement("div");
-    var qBtnC = document.createElement("div");
-    var qBtnD = document.createElement("div");
+    var btnDivA = document.createElement("div");
+    var btnDivB = document.createElement("div");
+    var btnDivC = document.createElement("div");
+    var btnDivD = document.createElement("div");
     var titleSection = document.createElement("h1");
     var textSection = document.createElement("p");
     var abtn = document.createElement("button")
@@ -35,106 +35,63 @@ document.getElementById("stbtn").addEventListener("click", function startQuizBut
     startEl.remove("homestart");
     // btnEl.remove("stbtn");
     
-    btnEl.appendChild(qBtnA);
-    btnEl.appendChild(qBtnB);
-    btnEl.appendChild(qBtnC);
-    btnEl.appendChild(qBtnD);
+    btnEl.appendChild(btnDivA);
+    btnEl.appendChild(btnDivB);
+    btnEl.appendChild(btnDivC);
+    btnEl.appendChild(btnDivD);
     
-    qBtnA.appendChild(abtn);
-    qBtnB.appendChild(bbtn);
-    qBtnC.appendChild(cbtn);
-    qBtnD.appendChild(dbtn);
+    btnDivA.appendChild(abtn);
+    btnDivA.className = "questions-buttons-a";
+    btnDivB.appendChild(bbtn);
+    btnDivB.className = "questions-buttons-b";
+    btnDivC.appendChild(cbtn);
+    btnDivC.className = "questions-buttons-c";
+    btnDivD.appendChild(dbtn);
+    btnDivD.className = "questions-buttons-d";
     
+    abtn.className = "a-btn";
+    abtn.textContent = quizStart[0].answers.a;
+    abtn.setAttribute("id", "answerA");
+    
+    bbtn.className = "b-btn";
+    bbtn.textContent = quizStart[0].answers.b;
+    bbtn.setAttribute("id", "answerB");
+    
+    cbtn.className = "c-btn";
+    cbtn.textContent = quizStart[0].answers.c;
+    cbtn.setAttribute("id", "answerC");
+    
+    dbtn.className = "d-btn";
+    dbtn.textContent = quizStart[0].answers.d;
+    dbtn.setAttribute("id", "answerD");
     
     titleSection.className = "h1-box";
-    titleSection.textContent = questionText;
+    titleSection.textContent = quizStart[0].question;
     
     textSection.className = "question-text";
     textSection.textContent = "";
-    
-    qBtnA.className = "questions-buttons-a";
-    qBtnB.className = "questions-buttons-b";
-    qBtnC.className = "questions-buttons-c";
-    qBtnD.className = "questions-buttons-d";
-    
-    
-    abtn.className = "a-btn";
-    abtn.textContent = asnwerbuttons.a;
-    abtn.setAttribute("id", "abtn");
-    
-    bbtn.className = "b-btn";
-    bbtn.textContent = asnwerbuttons.b;
-    bbtn.setAttribute("id", "abtn");
-    
-    cbtn.className = "c-btn";
-    cbtn.textContent = asnwerbuttons.c;
-    cbtn.setAttribute("id", "abtn");
-    
-    dbtn.className = "d-btn";
-    dbtn.textContent = asnwerbuttons.d;
-    dbtn.setAttribute("id", "abtn");
-    
-    document.getElementById("abtn").addEventListener("click", function() {
-            console.log("hello");
-        }
-    );
-    
-    // document.getElementById("bbtn").addEventListener("click", function() {
-    //     if (quizStart === true) {
-    //         console.log("hello")
-    //     }
-    // });
-    
-    // document.getElementById("cbtn").addEventListener("click", function() {
-    //     if (quizStart === true) {
-    //         console.log("hello")
-    //     }
-    // });
-    
-    // document.getElementById("dbtn").addEventListener("click", function() {
-    //     if (quizStart === true) {
-    //         console.log("hello")
-    //     }
-    // });
-})
 
-//loop through the array of questions when each question is asnwered, replacing the DOM elements with updated values
-for (var i = 0; i < quizStart.length; i++) {
-    var questionText = quizStart[i].question;
-    var asnwerbuttons = quizStart[i].answers;
-    if (quizStart[i].answers = quizStart[i].correctAnswer) {
+    document.getElementById("btn").addEventListener("click", function answerSelection() {
         console.log("hello");
-    }
-}
+    });
+});
+
+    
+
+// var questionText = quizStart[0].question;
+// var asnwerbuttons = quizStart[0].answers;
+// var correctResponse = quizStart[0].correctAnswer;
+// if (answerSelection() === correctResponse) {
+//     console.log("correct")
+// }
+//loop through the array of questions when each question is asnwered, replacing the DOM elements with updated values
+// for (var i = 0; i < quizStart.length; i++) {
+// }
 
 //each question should have a time limit of 60 seconds (60,000 ms)
 
 //when a questions answer is selected, it should log a true or false and replace the section with the html for the new question
-document.getElementById("stbtn").addEventListener("click", function answerSelection() {
 
-    document.getElementById("abtn").addEventListener("click", function() {
-            console.log("hello");
-        }
-    );
-    
-    // document.getElementById("bbtn").addEventListener("click", function() {
-    //     if (quizStart === true) {
-    //         console.log("hello")
-    //     }
-    // });
-    
-    // document.getElementById("cbtn").addEventListener("click", function() {
-    //     if (quizStart === true) {
-    //         console.log("hello")
-    //     }
-    // });
-    
-    // document.getElementById("dbtn").addEventListener("click", function() {
-    //     if (quizStart === true) {
-    //         console.log("hello")
-    //     }
-    // });
-});
 //each new question should display a boolean value for the previous question at the bottom
 
 //once all questions have been answered, or the timer on a question reaches 0, the user is prensented with thier score
